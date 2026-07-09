62b11a71a6eb49d0526be49f9245585f7fcb3c56
Ridhi Raheja <ridhiraheja3@gmail.com>
2026-07-09 14:50:19 +0530

    [268] [Time Beats: 100.00%] [Memory Beats: 70.77%] - LeetPush

---
+++ 268-Missing-Number.cpp
@@ -0,0 +1,13 @@
+class Solution {
+public:
+    int missingNumber(vector<int>& nums) {
+        int n = nums.size();
+        int expected_sum=n*(n+1)/2;
+        int actual_sum=0;
+        for(int num:nums){
+            actual_sum+=num;
+        }
+        int missing_number=expected_sum-actual_sum;
+        return missing_number;
+    }
+};
