effcc02d3b581c754e9c717e04b1d67298bb2797
Ridhi Raheja <ridhiraheja3@gmail.com>
2026-07-15 10:36:43 +0530

    [2870] [Time Beats: 30.67%] [Memory Beats: 95.46%] - LeetPush

---
+++ 2870-Minimum-Number-of-Operations-to-Make-Array-Empty.cpp
@@ -0,0 +1,25 @@
+class Solution {
+public:
+    int minOperations(vector<int>& nums) {
+        int n=nums.size();
+        unordered_map<int,int>mp;
+        for(int i=0;i<n;i++){
+            mp[nums[i]]++;
+        }
+        int result=0;
+        for(auto&it:mp){
+            int freq=it.second;
+            if(freq==1){
+                return -1;
+            }
+            while(freq%3!=0){
+                freq-=2;
+                result++;
+            }
+            result+=freq/3;
+
+        }
+        return result;
+        
+    }
+};
