de3ea15131d0082822a0fbe88c70a9b8038b2b40
Ridhi Raheja <ridhiraheja3@gmail.com>
2026-07-15 11:04:04 +0530

    [2405] [Time Beats: 71.54%] [Memory Beats: 77.59%] - LeetPush

---
+++ 2405-Optimal-Partition-of-String.cpp
@@ -0,0 +1,17 @@
+class Solution {
+public:
+    int partitionString(string s) {
+        vector<int> lastSeen(26,-1);
+        int count=0,substringStart=0;
+        for(int i=0;i<s.length();i++){
+            char ch=s[i];
+            if(lastSeen[ch-'a']>=substringStart){
+                count++;
+                substringStart=i;
+            }
+            lastSeen[ch-'a']=i;
+        }
+        return count+1;
+        
+    }
+};
