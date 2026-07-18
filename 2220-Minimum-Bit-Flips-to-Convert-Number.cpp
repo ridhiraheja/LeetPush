41861f65284128f8e127ca6da2c3c7a024764ea3
Ridhi Raheja <ridhiraheja3@gmail.com>
2026-07-18 12:22:37 +0530

    [2220] [Time Beats: 100.00%] [Memory Beats: 26.43%] - LeetPush

---
+++ 2220-Minimum-Bit-Flips-to-Convert-Number.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    int minBitFlips(int start, int goal) {
+        int x = start ^ goal;
+        int count = 0;
+
+        while (x > 0) {
+            if (x & 1)
+                count++;
+
+            x >>= 1;
+        }
+
+        return count;
+    }
+};
