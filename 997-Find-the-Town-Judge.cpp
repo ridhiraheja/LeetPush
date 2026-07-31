c5b876639e8ccd4206005456eec72a4909fa8164
Ridhi Raheja <ridhiraheja3@gmail.com>
2026-07-31 13:33:41 +0530

    [997] [Time Beats: 100.00%] [Memory Beats: 50.17%] - LeetPush

---
+++ 997-Find-the-Town-Judge.cpp
@@ -0,0 +1,22 @@
+class Solution {
+public:
+    int findJudge(int n, vector<vector<int>>& trust) {
+        vector<int> indegree(n + 1, 0);
+        vector<int> outdegree(n + 1, 0);
+
+        for (auto &e : trust) {
+            int u = e[0];
+            int v = e[1];
+
+            outdegree[u]++;
+            indegree[v]++;
+        }
+
+        for (int i = 1; i <= n; i++) {
+            if (indegree[i] == n - 1 && outdegree[i] == 0)
+                return i;
+        }
+
+        return -1;
+    }
+};
