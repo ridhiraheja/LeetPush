6cf97556e98075dbfad28ff5909f58426c52e6d2
Ridhi Raheja <ridhiraheja3@gmail.com>
2026-08-01 11:42:59 +0530

    [63] [Time Beats: 100.00%] [Memory Beats: 55.36%] - LeetPush

---
+++ 63-Unique-Paths-II.cpp
@@ -0,0 +1,21 @@
+class Solution {
+public:
+    //memorization
+    int solve(int i,int j,int m,int n,vector<vector<int>> &obstacleGrid, vector<vector<int>>&dp){
+        if(i>=m || j>=n || obstacleGrid[i][j]==1) return 0;        
+        if(i==m-1 && j==n-1) return 1;
+        if(dp[i][j]!=-1) return dp[i][j];
+        return dp[i][j]=solve(i+1,j,m,n,obstacleGrid,dp) + solve(i,j+1,m,n,obstacleGrid,dp);
+
+    }
+    int uniquePathsWithObstacles(vector<vector<int>>& obstacleGrid) {
+        int m=obstacleGrid.size();
+        int n=obstacleGrid[0].size();
+        vector<vector<int>> dp(m+1,vector<int>(n+1,-1));
+        return solve(0,0,m,n,obstacleGrid,dp);
+    
+    
+
+        
+    }
+};
