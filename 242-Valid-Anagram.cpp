24999cf31a3bdc6bd1b45c5a185dfcf0031e4950
Ridhi Raheja <ridhiraheja3@gmail.com>
2026-08-18 11:35:41 +0530

    [242] [Time Beats: 45.27%] [Memory Beats: 26.23%] - LeetPush

---
+++ 242-Valid-Anagram.cpp
@@ -0,0 +1,19 @@
+class Solution {
+public:
+    bool isAnagram(string s, string t) {
+        int freq1[26]={0};
+        int freq2[26]={0};
+        for(int i=0;i<s.size();i++){
+            freq1[s[i]-'a']++;
+        }
+        for(int i=0;i<t.size();i++){
+            freq2[t[i]-'a']++;
+        }
+        for(int i=0;i<26;i++){
+            if(freq1[i]!=freq2[i]) return false;
+        }
+        return true;
+
+        
+    }
+};
