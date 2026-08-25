52a3261b6c75a2ce585756e57e5365515525c9c8
Ridhi Raheja <ridhiraheja3@gmail.com>
2026-08-25 11:58:12 +0530

    [875] [Time Beats: 86.95%] [Memory Beats: 88.74%] - LeetPush

---
+++ 875-Koko-Eating-Bananas.cpp
@@ -0,0 +1,30 @@
+class Solution {
+public:
+    int minEatingSpeed(vector<int>& piles, int h) {
+        
+        int low = 1;
+        int high = *max_element(piles.begin(), piles.end());
+
+        while(low <= high) {
+            
+            int mid = low + (high - low) / 2;
+
+            long long hours = 0;
+
+            for(int pile : piles) {
+                hours += (pile + mid - 1) / mid;
+            }
+
+            if(hours <= h) {
+                // mid works, try smaller speed
+                high = mid - 1;
+            }
+            else {
+                // mid is too slow
+                low = mid + 1;
+            }
+        }
+
+        return low;
+    }
+};
