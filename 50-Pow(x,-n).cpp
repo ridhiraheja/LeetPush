bf4859f1f3152b716f0b5e536d9fa3e3a792b670
Ridhi Raheja <ridhiraheja3@gmail.com>
2026-09-25 02:56:59 +0530

    [50] [Time Beats: 100.00%] [Memory Beats: 93.13%] - LeetPush

--- 50-Pow(x,-n).cpp
+++ 50-Pow(x,-n).cpp
@@ -1,13 +1,20 @@
 class Solution {
 public:
-    double solve(double x,long n){
-        if(n==0) return 1;
-        if(n<0) return solve(1/x,-n);
-        if(n%2==0) return solve(x*x,n/2);
-        return x * solve(x*x,(n-1)/2);
-    }
     double myPow(double x, int n) {
-        return solve(x,(long)n);
-        
+        long long N = n;
+        if(N<0){
+            x=1/x;
+            N=-N;
+        }
+        double ans=1;
+        while(N>0){
+            if(N%2==1){
+                ans*=x;
+            }
+            x*=x;
+            N/=2;
+        }
+        return ans;
+
     }
 };
