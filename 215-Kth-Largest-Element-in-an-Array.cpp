432ede8e0916937b9725d3b74070d893412bfddd
Ridhi Raheja <ridhiraheja3@gmail.com>
2026-09-25 04:03:02 +0530

    [215] [Time Beats: 58.24%] [Memory Beats: 91.69%] - LeetPush

--- 215-Kth-Largest-Element-in-an-Array.cpp
+++ 215-Kth-Largest-Element-in-an-Array.cpp
@@ -1,14 +1,59 @@
 class Solution {
 public:
-    int findKthLargest(vector<int>& nums, int k) {
-        priority_queue<int,vector<int>,greater<int>>pq;
-        for(int i:nums){
-            pq.push(i);
-            if(pq.size()>k){
-                pq.pop();
+    int partition(vector<int>&nums,int l,int r){
+        int i=l+1;
+        int j=r;
+        int p=nums[l];
+        while(i<=j){
+            if(nums[i]<p && nums[j]>p){
+                swap(nums[i],nums[j]);
+                i++;
+                j--;
             }
+            if(nums[i]>=p) i++;
+            if(nums[j]<=p) j--;
+
         }
-        return pq.top();
-        
+        swap(nums[l],nums[j]);
+        return j;
+    }
+    int findKthLargest(vector<int>& nums, int k) {
+        int n=nums.size();
+        int l=0;
+        int r=n-1;
+        int pivot=0;
+        while(true){
+            pivot=partition(nums,l,r);
+            if(pivot==k-1) break;
+            if(pivot>k-1) r=pivot-1;
+            else l=pivot+1;
+        }
+        return nums[pivot];
     }
 };
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
